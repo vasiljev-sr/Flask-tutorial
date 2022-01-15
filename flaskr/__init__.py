5cb2da64920dc82809c77c8454300425ad8d695e
vasiljev-sr <ravil.vasilev@yandex.ru>
2022-01-15 23:15:08 +0300

    Добавил Blueprint (схемы) для  функций аутентификации для функций сообщений в блогах

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from . import db, auth
+from . import db, auth, blog
 
 
 def create_app(test_config=None):
@@ -32,6 +32,8 @@ def create_app(test_config=None):
 
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
 
     return app
