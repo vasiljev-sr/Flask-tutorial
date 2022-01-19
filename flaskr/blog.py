9a933e4683ab9339594839cac995d516d1bfbae5
Ravil Vasilev <ravil.vasilev@yandex.ru>
2022-01-19 10:18:46 +0300

    Добавил отображение текущей сделки

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -11,12 +11,13 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    order = db.execute(
+        'SELECT p.id, p.datetime, opened_position, entry_price,'
+        ' position_size, margin, profit, status ,symbol '
+        ' FROM positions p JOIN current_position u ON p.id = u.id'
+
+    ).fetchone()
+    return render_template('blog/index.html', order=order)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
